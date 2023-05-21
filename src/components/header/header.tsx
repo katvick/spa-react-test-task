09180252c34435b28d068a58c856b1f1870b8d37
Kate <stolyarova.katy@ya.ru>
2023-05-21 14:12:41 +0500

    рефакторит header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,42 +2,16 @@ import {
   Container,
   Row,
   Col,
-  Navbar,
-  Offcanvas,
-  Nav,
+
  } from 'react-bootstrap';
-import NavUser from '../nav-user/nav-user';
+import NavBar from '../nav-bar/nav-bar';
 
 function Header(): JSX.Element {
   return (
     <Container>
       <Row>
         <Col>
-        <Navbar bg="light" expand={false} className="mb-3 ">
-          <Container fluid>
-            {/* <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand> */}
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${false}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
-              placement="start"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${false}`}>
-                  Меню
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-start flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Список постов</Nav.Link>
-                  <Nav.Link href="#action2">Обо мне</Nav.Link>
-                  
-                  <NavUser />
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
+          <NavBar />
         </Col>
       </Row>
     </Container>
---
+++ src/components/nav-bar/nav-bar.tsx
@@ -0,0 +1,36 @@
+import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import NavUser from "../nav-user/nav-user";
+
+function NavBar(): JSX.Element {
+  return (
+    <Navbar bg="light" expand={false} className="mb-3 ">
+      <Container fluid>
+        {/* <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand> */}
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} />
+
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-${false}`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
+          placement="start"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${false}`}>
+              Меню
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          
+          <Offcanvas.Body>
+            <Nav className="justify-content-start flex-grow-1 pe-3">
+              <Nav.Link href="#action1">Список постов</Nav.Link>
+              <Nav.Link href="#action2">Обо мне</Nav.Link>
+              
+              <NavUser />
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar;
