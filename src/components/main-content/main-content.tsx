323d1122ee88f77124678834e5bbaab3d4ada7da
Kate <stolyarova.katy@ya.ru>
2023-05-22 14:36:20 +0500

    выделит список постов в отдельный компонент

--- src/components/main-content/main-content.tsx
+++ src/components/main-content/main-content.tsx
@@ -1,14 +1,13 @@
 import { Container } from 'react-bootstrap';
 import Post from '../post/post';
+import PostsListMain from '../posts-list-main/posts-list-main';
 
 function MainContent(): JSX.Element {
   return (
     <main className="main">
       <Container>
         <h1 className="display-5 mb-5">Список постов</h1>
-        <Post />
-        <Post />
-        <Post />
+        <PostsListMain />
       </Container>
     </main>
   )
