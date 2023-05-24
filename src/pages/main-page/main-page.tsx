28e425f472e6a889f70cebe5e4ba5fd43ead62df
Kate <stolyarova.katy@ya.ru>
2023-05-24 15:44:00 +0500

    добавит получение списка постов в компонент

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
 import Header from '../../components/header/header';
 import MainContent from '../../components/main-content/main-content';
+import { useAppDispatch } from '../../hooks';
+import { fetchPostsAction } from '../../store/api-actions';
 
 function MainPage(): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPostsAction());
+  }, [dispatch]);
+
   return (
     <>
       <Header />
--- src/store/posts/reducer.ts
+++ src/store/posts/reducer.ts
@@ -1,13 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { PostsState } from '../../types/state';
-import { Posts } from '../../mocks/posts';
-import { Comments } from '../../mocks/comments';
 import { NameSpace } from '../../const';
 import { loadPosts } from './actions';
 
 const initialState: PostsState = {
-  posts: Posts,
-  comments: Comments,
+  posts: [],
+  comments: [],
 }
 
 export const postsReducer = createSlice({
