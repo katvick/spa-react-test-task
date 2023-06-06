0286252eb0fe70f081cf6389bbbe864fdf59fb2c
Kate <stolyarova.katy@ya.ru>
2023-06-06 20:04:10 +0500

    добавит крестик для очистки поля и оптимизирует поиск

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,35 +1,26 @@
-import { Button, Form, InputGroup } from 'react-bootstrap';
+import { Form, InputGroup } from 'react-bootstrap';
 import { useAppDispatch } from '../../hooks';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent } from 'react';
 import { searchPosts } from '../../store/posts/reducer';
 
 function Search(): JSX.Element {
   const dispatch = useAppDispatch();
 
-  const [searchData, setSearchData] = useState('');
-
   const inputChangeHandle = ({target}: ChangeEvent<HTMLInputElement>) => {
     const {value} = target;
-    setSearchData(value);
-  }
-
-  const setSearchPhraseHandle = () => {
-    console.log(searchData);
-    dispatch(searchPosts({searchPhrase: searchData}))
+    dispatch(searchPosts({searchPhrase: value}))
   }
 
   return (
     <InputGroup className="mb-3 mx-auto" style={{ width: '80%' }}>
       <Form.Control
+        type="search"
         placeholder="Введите запрос"
         aria-label="Recipient's username"
         aria-describedby="basic-addon2"
         onChange={inputChangeHandle}
-        value={searchData}
-      />
-      <Button variant="outline-secondary" id="button-addon2" onClick={setSearchPhraseHandle}>
-        Поиск
-      </Button>
+      >
+      </Form.Control>
     </InputGroup>
   )
 }
