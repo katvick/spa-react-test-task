95de9be42f9e0a75f9f5e11f2c9e09029427aea8
Kate <stolyarova.katy@ya.ru>
2023-06-08 17:37:53 +0500

    фиксит мобильную верстку

--- src/components/about-info/about-info.tsx
+++ src/components/about-info/about-info.tsx
@@ -3,46 +3,47 @@ import { Card, Col, Image, Row, } from 'react-bootstrap';
 function AboutInfo(): JSX.Element {
   return (
         <Row className="my-4">
-          <Col className="d-flex align-items-start">
-            <div className="avatar avatar--about d-flex justify-content-center">
+          <Col md="3" className="px-4 mb-4">
+            <div className="avatar avatar--about d-flex">
               <Image
                 src='img/about.jpg'
                 roundedCircle
               />
             </div>
-            
-            <div className="about-info__content mx-5">
-              <Card.Title className="mb-3">
-                Столярова Екатерина
-              </Card.Title>
+          </Col>
 
-              <Card.Text>
-                <p>
-                  Я начинающий Frontend-разработчик. Мне очень нравится создавать красивые и удобные интерфейсы.
-                </p>
-                <p>
-                  Почему я решила этим заниматься?
-                </p>
-                <p>
-                  Долгое время работала в сфере маркетинга, разрабатывала интернет-рекламу. Давно думала о смене сферы деятельности. При обучении в университете заметила, что мне нравится работать с сайтами, их версткой и дизайном, нравится разбираться в их функционале. Какое-то время выбирала между дизайном и разработкой и остановилась на втором.
-                </p>
-                <p>
-                3 года назад начала обучение. За это время я прошла несколько курсов. Изучила много разных технологий:</p>
-                
-                <ul>
-                  <li>HTML, CSS</li>
-                  <li>JavaScript ES6</li>
-                  <li>TypeScript</li>
-                  <li>React, React+Redux, ReactRouter, ReactHooks, Redux toolkit</li>
-                  <li>npm, Webpack, Babel</li>
-                  <li>REST API</li>
-                </ul>
+          <Col className="px-4">
+            <Card.Title className="mb-3">
+              Столярова Екатерина
+            </Card.Title>
 
-                <p>
-                  Освоила систему контроля версий git. Верстала проекты с использованием БЭМ. Умею пользоваться графическими редакторами Figma, Photoshop.
-                </p>              
-              </Card.Text>
-            </div>
+            <Card.Text style={{fontSize: 16}}>
+              <p>
+                Я начинающий Frontend-разработчик. Мне очень нравится создавать красивые и удобные интерфейсы.
+              </p>
+              <p>
+                Почему я решила этим заниматься?
+              </p>
+              <p>
+                Долгое время работала в сфере маркетинга, разрабатывала интернет-рекламу. Давно думала о смене сферы деятельности. При обучении в университете заметила, что мне нравится работать с сайтами, их версткой и дизайном, нравится разбираться в их функционале. Какое-то время выбирала между дизайном и разработкой и остановилась на втором.
+              </p>
+              <p>
+              3 года назад начала обучение. За это время я прошла несколько курсов. Изучила много разных технологий:
+              </p>
+              
+              <ul>
+                <li>HTML, CSS</li>
+                <li>JavaScript ES6</li>
+                <li>TypeScript</li>
+                <li>React, React+Redux, ReactRouter, ReactHooks, Redux toolkit</li>
+                <li>npm, Webpack, Babel</li>
+                <li>REST API</li>
+              </ul>
+
+              <p>
+                Освоила систему контроля версий git. Верстала проекты с использованием БЭМ. Умею пользоваться графическими редакторами Figma, Photoshop.
+              </p>              
+            </Card.Text>
           </Col>
         </Row>
   )
--- src/components/user-card/user-card.tsx
+++ src/components/user-card/user-card.tsx
@@ -9,28 +9,28 @@ function UserCard({user}: UserCardProps): JSX.Element {
   return (
     <Card className='mx-auto mb-5 mt-3'>
       <Card.Body>
-        <Row className="my-3 mx-1">
-          <Col className="d-flex align-items-start">
+        <Row className="my-3 mx-3">
+          <Col md="3" className="d-flex justify-content-center mb-5">
             <div className="avatar avatar--user-card d-flex justify-content-center">
               <Image
                 src='https://m.buro247.ru/images/senina/aiony-haust-3TLl_97HNJo-unspl.jpg'
                 roundedCircle
               />
             </div>
-            
-            <div className="about-info__content mx-5">
-              <Card.Title className="mb-4">
-                {user?.name}
-              </Card.Title>
+          </Col>
+          
+          <Col>
+            <Card.Title className="mb-4">
+              {user?.name}
+            </Card.Title>
 
-              <Card.Text>
-                <p><b>Username:</b> {user?.username}</p>
-                <p><b>Email:</b> {user?.email}</p>
-                <p><b>Phone:</b> {user?.phone}</p>
-                <p><b>О компании:</b> {user?.company.name}. {user?.company.catchPhrase}. {user?.company.bs}</p>
-                <p><b>Сайт:</b> {user?.website}</p>
-              </Card.Text>
-            </div>
+            <Card.Text>
+              <p><b>Username:</b> {user?.username}</p>
+              <p><b>Email:</b> {user?.email}</p>
+              <p><b>Phone:</b> {user?.phone}</p>
+              <p><b>О компании:</b> {user?.company.name}. {user?.company.catchPhrase}. {user?.company.bs}</p>
+              <p><b>Сайт:</b> {user?.website}</p>
+            </Card.Text>
           </Col>
         </Row>
       </Card.Body>
