fbfddcd7cea68463867876573f1e73d638795d9a
Kate <stolyarova.katy@ya.ru>
2023-06-08 13:07:03 +0500

    добавит пагинацию

--- src/components/posts-list/posts-list.tsx
+++ src/components/posts-list/posts-list.tsx
@@ -1,15 +1,25 @@
 // import { useAppSelector } from '../../hooks';
+import { PaginationControl } from 'react-bootstrap-pagination-control';
 import { Post } from '../../types/user-data';
 import PostItem from '../post-item/post-item';
+import { useState } from 'react';
 
 type PostsListProps = {
   posts: Post[];
 }
 
 function PostsList({posts}: PostsListProps): JSX.Element {
+  const [page, setPage] = useState(1);
+
+  const limitPostsOnPage = 5;
+
+  const indexFirstActualPost = limitPostsOnPage * (page - 1);
+  const postsOnPage = posts.slice(indexFirstActualPost, indexFirstActualPost + 5);
+	console.log('TCL: postsOnPage', postsOnPage)
+
   return (
     <div className="posts-list-wrapper">
-      {posts.map((post) => {
+      {postsOnPage.map((post) => {
         const keyValue = `${post.id}-post`
         return (
           <PostItem
@@ -21,6 +31,18 @@ function PostsList({posts}: PostsListProps): JSX.Element {
           />
         )
       })}
+
+      <PaginationControl
+        page={page}
+        between={4}
+        total={posts.length}
+        limit={limitPostsOnPage}
+        changePage={(page) => {
+          setPage(page); 
+          console.log(page);
+        }}
+        ellipsis={1}
+      />
     </div>
   )
 }
